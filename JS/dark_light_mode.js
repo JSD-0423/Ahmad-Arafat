0ab9fe9d776d52ab8bf530957b518b3311d83542
Ahmad <ahmadjarafat@gmail.com>
2023-05-19 01:55:02 +0300

    add details page

--- JS/dark_light_mode.js
+++ JS/dark_light_mode.js
@@ -1,15 +1,5 @@
-const searchInput = document.getElementsByClassName("search-input")[0];
-
-searchInput.addEventListener("focus", (e) => {
-  e.target.value = "           ";
-});
-
-searchInput.addEventListener("input", (e) => {
-  console.log(e.target.value.length);
-  if (e.target.value < 11) e.target.value = "           ";
-});
-
 let isDark = false;
+let isFav = false;
 let root = document.querySelector(":root");
 
 const lightModeColors = {
@@ -20,19 +10,25 @@ const lightModeColors = {
   "lines-color": "#dddddd",
   "body-text": "#333333",
   "heart-color": "#dc143c",
+  shadow: "#dddddd",
+  "details-dark-section": "#282828",
 };
 
 const darkModeColors = {
   "brand-primary": "#0768AC",
   "brand-secondary": "#03C180",
-  "bg-default": "	#1A1A1A",
+  "bg-default": "#1A1A1A",
   "bg-body": "#282828",
   "lines-color": "#000000",
   "body-text": "#EDEDED",
   "heart-color": "#DC143C",
+  shadow: "#1A1A1A",
+  "details-dark-section": "#333333",
 };
 
 let darkModeButton = document.getElementsByClassName("icon-button")[0];
+
+//dark mode
 darkModeButton.addEventListener("click", () => {
   isDark = !isDark;
 
---
+++ JS/show_fav.js
@@ -0,0 +1,10 @@
+let favButton = document.getElementsByClassName("icon-button")[1];
+let favSection = document.getElementsByClassName("favourites")[0];
+
+// favourites show/hide
+favButton.addEventListener("click", () => {
+  isFav = !isFav;
+
+  if (isFav) favSection.style.bottom = "0px";
+  else favSection.style.bottom = "-178px";
+});
